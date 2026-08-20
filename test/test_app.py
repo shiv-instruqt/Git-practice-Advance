5141558ff9ebb8c45621c5144679413c60e33755
Shiv Tushal <shivtushal2004@gmail.com>
2026-08-20 16:45:14 +0530

    fix: handle division by zero

--- test/test_app.py
+++ test/test_app.py
@@ -10,3 +10,10 @@ def test_subtract():
 
 def test_multiply():
     assert multiply(3, 4) == 12
+
+def test_divide_by_zero():
+    try:
+        divide(10, 0)
+        assert False
+    except ZeroDivisionError:
+        assert True
